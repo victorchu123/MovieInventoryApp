cf0612ec787b57805d31bc5d32eb048763514c19
Victor <victorchu123@gmail.com>
2016-08-14 15:07:39 -0700

    edited views html files

--- MovieInventoryApp/Controllers/MoviesController.cs
+++ MovieInventoryApp/Controllers/MoviesController.cs
@@ -46,7 +46,7 @@ namespace MovieInventoryApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre")] Movie movie)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,IMDBRank")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace MovieInventoryApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre")] Movie movie)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,IMDBRank")] Movie movie)
         {
             if (ModelState.IsValid)
             {
